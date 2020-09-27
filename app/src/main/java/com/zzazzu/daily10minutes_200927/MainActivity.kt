fc0d4cd19f86b5a946b20d3da29520af98400f13
bucephalus41 <bucephalus41@gmail.com>
2020-09-27 15:45:45 +0900

    로그인 클릭시 id/pw 받아내기

--- app/src/main/java/com/zzazzu/daily10minutes_200927/MainActivity.kt
+++ app/src/main/java/com/zzazzu/daily10minutes_200927/MainActivity.kt
@@ -2,6 +2,7 @@ package com.zzazzu.daily10minutes_200927
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +13,14 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        loginBtn.setOnClickListener{
+
+// inputid 통해 받은 텍스트를 스트링으로 바꾸자라는 내
+            val inputId = idEdt.text.toString()
+            val inputId = pwEdt.text.toString()
+        }
+
 
     }
 

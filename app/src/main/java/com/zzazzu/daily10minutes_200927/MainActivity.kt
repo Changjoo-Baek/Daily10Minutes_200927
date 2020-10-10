6341853c27ec9975ce97cdc86ab2c7a64c33eac0
bucephalus41 <bucephalus41@gmail.com>
2020-10-10 11:27:19 +0900

    로그인 실패시 토스트 출력

--- app/src/main/java/com/zzazzu/daily10minutes_200927/MainActivity.kt
+++ app/src/main/java/com/zzazzu/daily10minutes_200927/MainActivity.kt
@@ -4,6 +4,7 @@ package com.zzazzu.daily10minutes_200927
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.zzazzu.daily10minutes_200927.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
@@ -30,7 +31,26 @@ class MainActivity : BaseActivity() {
             ServerUtil.postRequestLogin(inputId, inputPw, object : ServerUtil.JsonResponseHandler {
                 override fun onResponse(json: JSONObject) {
 
-                    Log.d("메인화면", "로그인 응답 확인")
+//                    로그인 실행 결과에 따라 행동할 내용을 적는 공간.
+//                    code라는 이름으로 적힌 Int값을 받아서, 200이냐 아니냐에 따라 다른 행동.
+
+                    val codeNum = json.getInt("code")
+
+                    Log.d("서버가 알려주는 코드", codeNum.toString())
+
+                    if (codeNum == 200) {
+
+                    }
+                    else {
+//                        로그인 실패 => 토스트로 로그인 실패 안내.
+//                        토스트 : UI 동작 -> UI Thread가 실행하도록 해야 함.
+
+                        runOnUiThread {
+                            Toast.makeText(mContext, "로그인 실패", Toast.LENGTH_SHORT)
+                        }
+
+                    }
+
                 }
             })
 

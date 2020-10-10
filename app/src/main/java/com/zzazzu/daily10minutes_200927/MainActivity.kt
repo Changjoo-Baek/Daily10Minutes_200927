e284bc5f64977788e8871061af5860e75eac4f86
bucephalus41 <bucephalus41@gmail.com>
2020-10-10 11:01:02 +0900

    자꾸 앱이 꺼짐

--- app/src/main/java/com/zzazzu/daily10minutes_200927/MainActivity.kt
+++ app/src/main/java/com/zzazzu/daily10minutes_200927/MainActivity.kt
@@ -2,12 +2,11 @@ package com.zzazzu.daily10minutes_200927
 
 
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.zzazzu.daily10minutes_200927.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_main.*
-
-
+import org.json.JSONObject
 
 
 class MainActivity : BaseActivity() {
@@ -24,11 +23,18 @@ class MainActivity : BaseActivity() {
 
 // inputid 통해 받은 텍스트를 스트링으로 바꾸자라는 내
             val inputId = idEdt.text.toString()
-            val inputpw = pwEdt.text.toString()
+            val inputPw = pwEdt.text.toString()
 
 // 서버 유틸을 이용해서 실제 로그인 시도
 
-            ServerUtil.postRequestLogin(inputId, inputpw)
+            ServerUtil.postRequestLogin(inputId, inputPw, object : ServerUtil.JsonResponseHandler {
+                override fun onResponse(json: JSONObject) {
+
+                    Log.d("메인화면", "로그인 응답 확인")
+                }
+            })
+
+//
         }
 
 
--- app/src/main/java/com/zzazzu/daily10minutes_200927/utils/ServerUtil.kt
+++ app/src/main/java/com/zzazzu/daily10minutes_200927/utils/ServerUtil.kt
@@ -1,18 +1,34 @@
 package com.zzazzu.daily10minutes_200927.utils
 
-import okhttp3.FormBody
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import android.util.Log
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
 
 class ServerUtil {
 
+
+// 화면(액티비티)의 입장에서, 서버응답이 들어왔을 때 어떤 행동을 실행할지
+// 행동 지침을 담아주기 위한 인터페이스 (가이드북/ 매뉴얼) 정
 // companion object 는 다른 파일에서 serverutil.porst~ 요렇게 바로 쓸 수 ㅣㅇㅆ게 해
+
+    interface JsonResponseHandler {
+        fun onResponse(json : JSONObject)
+
+
+
+    }
+
 
     companion object {
 
         val HOST_URL = "http://15.164.153.174"
 
-        fun postRequestLogin(id:String, pw:String) {
+        fun postRequestLogin(
+            id: String,
+            pw: String,
+            handler: JsonResponseHandler?
+        ) {
 
             val client = OkHttpClient()
 
@@ -36,7 +52,34 @@ class ServerUtil {
 // 먼저 어디로 갈 건가요 쓰고, 어떻게 갈 건가요, 뭘 들고 갈건요 쓰는 순서이다. 그리고나서 빌드 해주면 리퀘스트 완
 // 완성된 request를 가지고 실제 서버 연결 코드
 
-            client.newCall(request)
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+//                    서버에 연결 자체를 실패한 경우, (물리적 실패)
+//                    인터넷 연결 실패 등등.
+//                    아이디/비번 틀림 등 로그인 실패는 여기가 아님
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+
+//                    검사 결과가 성공이던, 실패던 관계 없이 서버가 뭔가 답변을 해주면 무조건 실행.
+//                    서버가 내려준 응답 중 본문(body) 만 string 형태로 저장.
+
+                    val bodyString = response.body!!.string()
+
+//                    받아낸 string을 => 분석하기 용이한 jsonobject로 변환.
+
+                    val jsonObj = JSONObject(bodyString)
+
+                    Log.d("서버응답본문", jsonObj.toString())
+
+//                    어떤 서버응답 처리를 해줄 지 가이드북 (인터페이스가 존재한다면,
+//                    그 가이드북에 적힌 내용을 실제로 실행해달라는 코드.
+
+                    handler?.onResponse(jsonObj)
+
+                }
+
+            })
         }
 
     }
